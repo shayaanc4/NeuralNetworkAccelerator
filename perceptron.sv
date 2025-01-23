48e18ccc669bc1a45b21ced0f634ba9187a7f0f0
shayaanc4 <73558042+shayaanc4@users.noreply.github.com>
2025-01-22 20:41:47 -0500

    Update perceptron.sv formatting

--- perceptron.sv
+++ perceptron.sv
@@ -1,14 +1,19 @@
 module perceptron (clk, rst, en, data_in, weight, sum);
-	
+
+	// Parameters
 	parameter integer NUM_INPUTS = 784;
 	parameter integer INPUT_WIDTH = 16;
 	parameter integer WEIGHT_WIDTH = 16;
-	
+
+	// Inputs
 	input logic clk, rst, en;
 	input logic signed [7:0] data_in; // (3,5)
 	input logic signed [7:0] weight; // (3,5)
+
+	// Output
 	output logic signed [12:0] sum; // (8,5)
-	
+
+	// Internal
 	logic signed [15:0] mult; // (6,10)
 	logic signed [10:0] mult_trunc; // (6,5)
 	
