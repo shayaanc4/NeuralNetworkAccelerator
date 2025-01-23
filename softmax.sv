1305b1c2267bfdc012286906190d2a4304a828be
shayaanc4 <73558042+shayaanc4@users.noreply.github.com>
2025-01-22 20:43:37 -0500

    Update softmax.sv formatting

--- softmax.sv
+++ softmax.sv
@@ -1,19 +1,25 @@
 module softmax (activations, max_index);
-	
+
+	// Parameter
 	parameter NUM_CLASSES = 10;
-	input logic signed [31:0] activations [0:NUM_CLASSES-1]; // Input array of activations
-	output logic [3:0] max_index;       // Output index of the maximum value
-	
-	logic signed [31:0] max_value;  // To store the maximum value
+
+	// Input
+	input logic signed [31:0] activations [0:NUM_CLASSES-1];
+
+	// Output
+	output logic [3:0] max_index;       
+
+	// Internal
+	logic signed [31:0] max_value;
 	
 	integer i;
 	always_comb begin
-	  max_value = activations[0];  // Initialize with the first element
+	  max_value = activations[0];  
 	  max_index = 0;
 	  for (i = 1; i < NUM_CLASSES; i++) begin
 		if (activations[i] > max_value) begin
-			 max_value = activations[i]; // Update max value
-			 max_index = i[3:0];  // Update index
+			 max_value = activations[i]; 
+			 max_index = i[3:0]; 
 		end
 	  end
 	end
